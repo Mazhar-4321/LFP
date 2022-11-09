ef884385de76870cfd25c90ae226bc7043e58d84
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-09 21:48:44 +0530

    [numberToWords] program to convert Number To Words

--- Day2/Main.java
+++ Day2/Main.java
@@ -1,4 +1,24 @@
 package LFP.Day2;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
+    static void numberInWords(int n){
+        String s="";
+        switch(n){
+            case 0:s="Zero";break;
+            case 1:s="One";break;
+            case 2:s="Two";break;
+            case 3:s="Three";break;
+            case 4:s="Four";break;
+            case 5:s="Five";break;
+            case 6:s="Six";break;
+            case 7:s="Seven";break;
+            case 8:s="Eight";break;
+            case 9:s="Nine";break;
+            default:s="Invalid";
+        }
+        System.out.println(s);
+    }
 }
