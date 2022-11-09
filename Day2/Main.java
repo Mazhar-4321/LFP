da45b334f99b585b9c5b038a6da8242326d1bc91
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-09 21:57:06 +0530

    [reverseOfANumber] program to find reverse of a Number

--- Day2/Main.java
+++ Day2/Main.java
@@ -29,4 +29,12 @@ public class Main {
         }
         System.out.println(sum);
     }
+    static void reverseOfANumber(int n){
+        int rev=0;
+        while (n>0){
+            rev+=rev*10+n%10;
+            n=n/10;
+        }
+        System.out.println(rev);;
+    }
 }
