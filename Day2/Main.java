107bc1afb4bfaf4d9f2b94d0610688a090f228b8
Mazhar-4321 <90505530+Mazhar-4321@users.noreply.github.com>
2022-11-10 06:25:10 +0530

    [sumOfNaturalNumbers] updated logic

--- Day2/Main.java
+++ Day2/Main.java
@@ -32,7 +32,7 @@ public class Main {
     static void reverseOfANumber(int n){
         int rev=0;
         while (n>0){
-            rev+=rev*10+n%10;
+            rev=rev*10+n%10;
             n=n/10;
         }
         System.out.println(rev);;
