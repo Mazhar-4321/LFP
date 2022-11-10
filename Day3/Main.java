83ce921279abc076723709be486d7c8a5f2e4ef3
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-10 21:53:11 +0530

    [elementsOfArray]

---
+++ Day3/Main.java
@@ -0,0 +1,12 @@
+package LFP.Day3;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+    }
+    static void elementsOfArray(int[] arr){
+        for(int x:arr){
+            System.out.println(x+" ");
+        }
+    }
+}
