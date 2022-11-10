60a707f07a83a626ca05dca32435ac6e84755cef
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-10 06:49:49 +0530

    [vowelOrConsonant] added program for vowel or consonant

--- Day2/Main.java
+++ Day2/Main.java
@@ -65,4 +65,14 @@ public class Main {
         else
             return false;
     }
+    static boolean isVowel(char c){
+        switch(c){
+            case 'a':return true;
+            case 'e': return true;
+            case 'i':return true;
+            case '0': return true;
+            case 'u':return true;
+            default: return false;
+        }
+    }
 }
