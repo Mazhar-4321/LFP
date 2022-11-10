237c260d9fb84dd6ed61691946fc2e5067f55afe
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-10 22:04:23 +0530

    [smallestElementOfArray]

--- Day3/Main.java
+++ Day3/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5,6};
         largestElementOfArray(arr);
+        smallestElementOfArray(arr);
     }
     static void elementsOfArray(int[] arr){
         for(int x:arr){
@@ -32,5 +33,14 @@ public class Main {
                     }
                 }
         System.out.println("max="+max);
+    }
+    static void smallestElementOfArray(int[] arr){
+        int min=arr[0];
+        for(int x: arr){
+            if(x<min){
+                min=x;
+            }
+        }
+        System.out.println("min="+min);
     }
 }
