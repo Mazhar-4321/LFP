cedc4f2eea7461b0da011d99dddecd51034f086c
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-10 06:46:11 +0530

    [palindromeNumber] added program for palindrome number

--- Day2/Main.java
+++ Day2/Main.java
@@ -52,4 +52,17 @@ public class Main {
         }
         System.out.println(rev);
     }
+    static boolean palindromeNumber(int n){
+        int temp=n;
+        int rev=0;
+        while (n>0){
+            rev=rev*10+n%10;
+            n=n/10;
+        }
+        if(rev==temp){
+           return true;
+        }
+        else
+            return false;
+    }
 }
