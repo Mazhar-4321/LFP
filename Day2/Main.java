c645488d00dd16f9c240ecc72c37e39ce27bc525
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-10 07:24:11 +0530

    [monthNameOfAGivenNumber] added program for month of a given number

--- Day2/Main.java
+++ Day2/Main.java
@@ -75,4 +75,22 @@ public class Main {
             default: return false;
         }
     }
+    static String monthNameOfANumber(int n){
+        switch (n){
+            case 1 : return "January";
+            case 2 : return "Februaru";
+            case 3 : return "March";
+            case 4 : return "April";
+            case 5 : return "May";
+            case 6 : return "June";
+            case 7 : return "July";
+            case 8 : return "August";
+            case 9 : return "September";
+            case 10 : return "October";
+            case 11 : return "November";
+            case 12 : return "December";
+            default:return "Invalid Number";
+        }
+    }
+
 }
