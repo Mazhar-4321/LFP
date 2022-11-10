04be2933683476a76254147808b2c720e1d6068a
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-10 22:22:59 +0530

    [duplicateElementsOfArray]

--- Day3/Main.java
+++ Day3/Main.java
@@ -8,6 +8,7 @@ public class Main {
         elementsAtEvenPosition(arr);
         elementsOfArrayInReverseOrder(arr);
         elementsAtOddPosition(arr);
+        duplicateElementsOfArray(arr);
     }
     static void elementsOfArray(int[] arr){
         for(int x:arr){
@@ -69,5 +70,21 @@ public class Main {
             c++;
         }
 
+    }
+    static void duplicateElementsOfArray(int[] arr){
+        for(int i=0;i< arr.length;i++){
+            boolean flag=false;
+            if (arr[i]==-1)
+                continue;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    arr[j]=-1;
+                    flag=true;
+                }
+            }
+            if(flag){
+                System.out.print(arr[i]+" ");
+            }
+        }
     }
 }
